7b0e815d07e1a072566f6630c97631e84819c2e7
Jeff Frasca <jeff@papertrail.com>
2017-09-05 14:59:43 -0700

    Initial pbuilderization of kafka10 package (build_kafka.sh now builds a source package ready to be pbuildered)

--- build_kafka.sh
+++ build_kafka.sh
@@ -20,36 +20,39 @@ if [ ! -f "${src_package}" ]; then
   wget ${download_url}
 fi
 mkdir -p tmp && pushd tmp
-rm -rf kafka
-mkdir -p kafka
-cd kafka
-mkdir -p build/usr/lib/kafka
-mkdir -p build/etc/default
-mkdir -p build/etc/init
-mkdir -p build/etc/kafka
-mkdir -p build/var/log/kafka
+TMPDIR=$(pwd)
+rm -rf kafka-$version
+mkdir -p kafka-$version
+#cd kafka-$version
+mkdir -p kafka-${version}/usr/lib/kafka
+mkdir -p kafka-${version}/etc/default
+mkdir -p kafka-${version}/etc/init
+mkdir -p kafka-${version}/etc/kafka
+mkdir -p kafka-${version}/var/log/kafka
+mkdir -p kafka-${version}/debian
 
-cp ${origdir}/kafka-broker.default build/etc/default/kafka-broker
-cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
+cp ${origdir}/kafka-broker.default kafka-${version}/etc/default/kafka-broker
+cp ${origdir}/kafka-broker.upstart.conf kafka-${version}/etc/init/kafka-broker.conf
+cp ${origdir}/debian/* kafka-${version}/debian
 
 tar zxf ${origdir}/${src_package}
 cd kafka_${scala_version}-${version}
-mv config/log4j.properties config/server.properties ../build/etc/kafka
-mv * ../build/usr/lib/kafka
-cd ../build
+mv config/log4j.properties config/server.properties ../kafka-${version}/etc/kafka
+mv * ../kafka-${version}/usr/lib/kafka
+cd ../kafka-${version}
 
-fpm -t deb \
-    -n ${name} \
-    -v ${version}${package_version} \
-    --description "${description}" \
-    --url="{$url}" \
-    -a ${arch} \
-    --category ${section} \
-    --vendor "" \
-    --license "${license}" \
-    -m "${USER}@localhost" \
-    --prefix=/ \
-    -s dir \
-    -- .
-mv kafka*.deb ${origdir}
+#fpm -t deb \
+#    -n ${name} \
+#    -v ${version}${package_version} \
+#    --description "${description}" \
+#    --url="{$url}" \
+#    -a ${arch} \
+#    --category ${section} \
+#    --vendor "" \
+#    --license "${license}" \
+#    -m "${USER}@localhost" \
+#    --prefix=/ \
+#    -s dir \
+#    -- .
+#mv kafka*.deb ${origdir}
 popd
