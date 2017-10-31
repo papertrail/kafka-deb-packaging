4957ae1f257e20f8c9f2dc46a288a37c69607c90
Jeff Frasca <jeff@papertrail.com>
2017-10-31 13:31:51 -0700

    Rearrange init scripts and add systemd service

--- build_kafka.sh
+++ build_kafka.sh
@@ -14,6 +14,26 @@ src_package="kafka_${scala_version}-${version}.tgz"
 download_url="http://mirror.cc.columbia.edu/pub/software/apache/kafka/${version}/${src_package}"
 origdir="$(pwd)"
 
+DISTRIB_CODENAME=""
+if [ -e /etc/lsb-release ]; then
+  source /etc/lsb-release
+fi
+
+case "${1:-$DISTRIB_CODENAME}" in
+  trusty)
+    version_tag="~u1404"
+    fpm_init_opts="--deb-upstart ../../../upstart/kafka-broker.conf"
+  ;;
+  xenial)
+    version_tag="~u1604"
+    fpm_init_opts="--deb-systemd ../../../systemd/kafka.service"
+  ;;
+  *)
+    echo "Unrecognized host distribution or codename unspecified (try '$0 xenial' or '$0 trusty')!"
+    exit 1
+  ;;
+esac
+
 #_ MAIN _#
 rm -rf ${name}*.deb
 if [ ! -f "${src_package}" ]; then
@@ -29,9 +49,6 @@ mkdir -p build/etc/init
 mkdir -p build/etc/kafka
 mkdir -p build/var/log/kafka
 
-cp ${origdir}/kafka-broker.default build/etc/default/kafka-broker
-cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
-
 tar zxf ${origdir}/${src_package}
 cd kafka_${scala_version}-${version}
 mv config/log4j.properties config/server.properties ../build/etc/kafka
@@ -40,7 +57,7 @@ cd ../build
 
 fpm -t deb \
     -n ${name} \
-    -v ${version}${package_version} \
+    -v ${version}${version_tag}${package_version} \
     --description "${description}" \
     --url="{$url}" \
     -a ${arch} \
@@ -49,6 +66,7 @@ fpm -t deb \
     --license "${license}" \
     -m "${USER}@localhost" \
     --prefix=/ \
+    ${fpm_init_opts} \
     -s dir \
     -- .
 mv kafka*.deb ${origdir}
